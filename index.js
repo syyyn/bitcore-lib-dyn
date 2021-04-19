dd8cb5b34b068e7bf42e3de2a7549069b156d5d8
Matthew Langdon <syyyn84@gmail.com>
2021-04-19 13:23:32 -0400

    Initial changes

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ bitcore.deps.bnjs = require('bn.js');
 bitcore.deps.bs58 = require('bs58');
 bitcore.deps.Buffer = Buffer;
 bitcore.deps.elliptic = require('elliptic');
-bitcore.deps.nodeArgon2d = require('multi-hashing');
+bitcore.deps.nodeArgon2d = require('node-hash-argon2d');
 bitcore.deps._ = require('lodash');
 
 // Internal usage, exposed for testing/advanced tweaking
